6e77c8ff6d3027a6874ae3a6d4fb7e1ad7ea18df
fax117 <38862497+fax117@users.noreply.github.com>
2019-09-04 02:57:03 -0500

    Pruebas
    don't try this at home...

---
+++ Untitled Cooking Sim/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private float xSpeed;
+    private float zSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        xSpeed = Input.GetAxis("Horizontal");
+        zSpeed = Input.GetAxisRaw("Vertical");
+        gameObject.transform.Translate(new Vector3(xSpeed,0f,zSpeed));
+    }
+}
