067d16de305d619a9111436baa74e2f5c3ea8295
Julieta Galvan <a01654311@itesm.mx>
2019-11-19 10:46:40 -0600

    Monster material & ingredient Y rotation

--- Untitled Cooking Sim/Assets/Scripts/Ingredient.cs
+++ Untitled Cooking Sim/Assets/Scripts/Ingredient.cs
@@ -15,6 +15,6 @@ public class Ingredient : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
     }
 }
