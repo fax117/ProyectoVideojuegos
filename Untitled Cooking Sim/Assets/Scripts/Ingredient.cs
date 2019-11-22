c1379f62964e02a0b385faca6a29bcd2d2e57007
Julieta Galvan <a01654311@itesm.mx>
2019-11-21 18:57:43 -0600

    Y Rotation for ingredients

--- Untitled Cooking Sim/Assets/Scripts/Ingredient.cs
+++ Untitled Cooking Sim/Assets/Scripts/Ingredient.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class Ingredient : MonoBehaviour
 {
-    public GameObject ingredient;
-    public Collider ingredientCollider;
-    // Start is called before the first frame update
-    void Start()
-    {
-        ingredientCollider = ingredient.GetComponent<Collider>();
-    }
+   
 
     // Update is called once per frame
     void Update()
